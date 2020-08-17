dd65520ddde3d30e436b0ccd2ecb41826841412f
antonina_n <antonina.nevstedt@consid.se>
2020-08-17 16:11:33 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+/*
+class App extends React.Component {
+  render() {
+    return <div>Hello, world!</div>;
+  }
+}
+*/
+
+function App() {
+  return <div>Hello, world!</div>;
+}
+
+export default App;
