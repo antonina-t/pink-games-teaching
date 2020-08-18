d18392080bead984b2fe117df8ea474dc92fcb04
antonina_n <antonina.nevstedt@consid.se>
2020-08-18 23:24:16 +0200

    Exercise Memory: Part 1

--- src/components/memory/index.js
+++ src/components/memory/index.js
@@ -31,9 +31,22 @@ function generateCards() {
   return cards.sort(() => Math.random() - 0.5);
 }
 
-function Memory() {
-  const [cards, setCards] = useState(generateCards());
+/* 
+Returns a new array of cards where the specified card (cardToFlip)
+will have a different value of its isFlipped: true changes to false and false to true.
+*/
+function flipCard(cards, cardToFlip) {
+  return cards.map((card) => {
+    if (cardToFlip.key === card.key)
+      return { ...card, isFlipped: !card.isFlipped };
+    return card;
+  });
+}
 
+function Memory() {
+  const [game, setGame] = useState({ cards: generateCards() });
+
+  /*
   function onCardClicked(clickedCard) {
     setCards((oldCards) => {
       return oldCards.map((card) => {
@@ -44,16 +57,66 @@ function Memory() {
       });
     });
   }
+  */
+
+  function onCardClicked(card) {
+    // If the card is already flipped there is nothing we need to do (write an if-statement with a return; inside)
+    if (card.isFlipped) return;
+
+    setGame(({ cards, firstCard, secondCard }) => {
+      // The { cards, firstCard, secondCard } above is the decomposed game object.
+      // These three variables represent the previous state, before a card was clicked.
+      // We should return the new state, depending on the previous one and on the card that was clicked.
+      // There are 4 different cases.
+      // 1. If both firstCard and secondCard from the previous state are undefined =>
+      // we should flip the clicked card and set it as the firstCard
+      if (!firstCard) {
+        return {
+          cards: flipCard(cards, card),
+          firstCard: card,
+        };
+      }
+      // 2. Else, if firstCard is defined, but secondCard isn't =>
+      // we should flip the clicked card, keep the firstCard as is, but set the secondCard
+      else if (!secondCard) {
+        return {
+          cards: flipCard(cards, card),
+          firstCard: firstCard,
+          secondCard: card,
+        };
+      }
+      // 3. Else, if the previous two clicked cards have the same color =>
+      // we should flip the clicked card, set the new firstCard and remove secondCard from the state
+      else if (firstCard.color === secondCard.color) {
+        return {
+          cards: flipCard(cards, card),
+          firstCard: card,
+        };
+      }
+      // 4. Else, if the previous two clicked cards have different colors =>
+      // we should flip the clicked card and flip back firstCard and secondCard,
+      // we should also set the new firstCard and remove secondCard from the state
+      else {
+        let newCards = flipCard(cards, firstCard);
+        newCards = flipCard(newCards, secondCard);
+        newCards = flipCard(newCards, card);
+        return {
+          cards: newCards,
+          firstCard: card,
+        };
+      }
+    });
+  }
 
   function onRestart() {
-    setCards(generateCards());
+    setGame({ cards: generateCards() });
   }
 
   return (
     <div className="game-container">
       <StatusBar status="Time: 0s" onRestart={onRestart}></StatusBar>
       <div className="memory-grid">
-        {cards.map((card) => (
+        {game.cards.map((card) => (
           <MemoryCard
             key={card.key}
             color={card.color}
