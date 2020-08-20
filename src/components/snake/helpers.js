0685afb0e8c081e8fe2960faaa8e9db6c0217b37
antonina_n <antonina.nevstedt@consid.se>
2020-08-20 13:55:30 +0200

    Snake Part 1.1

---
+++ src/components/snake/helpers.js
@@ -0,0 +1,24 @@
+const width = 20;
+const height = 12;
+
+export function generateGame() {
+  const snake = {
+    head: {
+      x: width / 2,
+      y: height / 2,
+    },
+    tail: [
+      {
+        x: width / 2 - 1,
+        y: height / 2,
+      },
+    ],
+    dir: "right",
+  };
+  return {
+    snake: snake,
+    food: generateFood(snake),
+  };
+}
+
+export function generateFood(snake) {}
--- src/components/snake/index.js
+++ src/components/snake/index.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import "./index.css";
+import * as helpers from "./helpers.js";
+
+const width = 20;
+const height = 12;
 
 function Snake() {
+  const [game, setGame] = useState();
+
+  const cells = [];
+  for (let i = 0; i < width * height; i++) {
+    cells.push(<div key={i} className="snake-cell"></div>);
+  }
+
   return (
-    <div className="text-container">
-      <h2>Snake game will be here...</h2>
+    <div className="game-container">
+      <div className="snake-grid">{cells}</div>
     </div>
   );
 }
