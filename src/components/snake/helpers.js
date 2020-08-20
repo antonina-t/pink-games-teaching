883caa2e72f291ed044091892fcfe075a483471f
antonina_n <antonina.nevstedt@consid.se>
2020-08-20 22:28:17 +0200

    Snake Part 1 Exercise

--- src/components/snake/helpers.js
+++ src/components/snake/helpers.js
@@ -43,6 +43,59 @@ function random(max) {
   return Math.floor(Math.random() * max);
 }
 
-export function tick(game) {
-  return { ...game, food: generateFood(game.snake) };
+export function tick(oldGame) {
+  const oldSnake = oldGame.snake;
+  const oldFood = oldGame.food;
+
+  const newHead = generateNewHead(oldSnake);
+  const newTail = generateNewTail(oldSnake, oldFood, newHead);
+  const newSnake = {
+    ...oldSnake,
+    head: newHead,
+    tail: newTail,
+  };
+
+  let newFood = oldFood;
+  // Wait a minute... if newHead has eaten the food, we should generate new food!
+  // In that case, change newFood, use generateFood function.
+  // --> your code here
+
+  return {
+    snake: newSnake,
+    food: newFood,
+  };
+}
+
+function generateNewHead(oldSnake) {
+  let newHead;
+  switch (oldSnake.dir) {
+    case "right":
+      newHead = { x: oldSnake.head.x + 1, y: oldSnake.head.y };
+      break;
+    case "down":
+      // newHead = ???
+      // --> your code here
+      break;
+    case "left":
+      // newHead = ???
+      // --> your code here
+      break;
+    case "up":
+      // newHead = ???
+      // --> your code here
+      break;
+  }
+  return newHead;
+}
+
+function generateNewTail(oldSnake, oldFood, newHead) {
+  // Create a variable newTail (an array). Its first cell should be the old snake's head
+  // and the rest of the cells should be the old snake's tail. Use concat() function
+  // to add (append) a whole array to another array. Or you can use the [...myArray] syntax somehow... :)
+  // --> your code here
+  // Now the snake's tail has become longer! We should keep it like that if the snake has eaten,
+  // otherwise we need to shorten it (remove the last element). Use the pop() function.
+  // --> your code here
+  // Don't forget to return newTail!
+  // --> your code here
 }
