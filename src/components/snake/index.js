08209d2745ba8188212fef3877942604a057e68e
antonina_n <antonina.nevstedt@consid.se>
2020-08-20 14:55:00 +0200

    Snake Part 1.3

--- src/components/snake/index.js
+++ src/components/snake/index.js
@@ -6,7 +6,7 @@ const width = 20;
 const height = 12;
 
 function Snake() {
-  const [game, setGame] = useState(helper.generateGame());
+  const [game, setGame] = useState(helpers.generateGame());
 
   const cells = [];
   for (let y = 0; y < height; y++) {
@@ -15,14 +15,18 @@ function Snake() {
       const cell = { x, y };
       let className = "";
       if (helpers.isEqual(cell, game.snake.head)) {
-        className = "head";
-      } else if () {
-        className = "food";
-      } else if () {
-        className = "tail";
+        className = " head";
+      } else if (helpers.isEqual(cell, game.food)) {
+        className = " food";
+      } else if (
+        game.snake.tail.some((tailCell) => helpers.isEqual(cell, tailCell))
+      ) {
+        className = " tail";
       }
 
-      cells.push(<div key={x + "-" + y} className="snake-cell"></div>);
+      cells.push(
+        <div key={x + "-" + y} className={"snake-cell" + className}></div>
+      );
     }
   }
 
